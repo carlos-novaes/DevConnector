31464b065035318906b30024539ab7855552cb6f
Carlos Novaes <carlosnovaes@poli.ufrj.br>
2018-12-29 12:41:23 -0200

    add validation to create and update profile routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,8 +4,10 @@ const router = express.Router();
 // @route GET api/posts/test
 // @desc Test post route
 // @access Public
-router.get("/test", (req, res) => res.json({
+router.get("/test", (req, res) =>
+  res.json({
     msg: "Posts works"
-}));
+  })
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,42 +1,46 @@
 const express = require("express"),
-    mongoose = require("mongoose"),
-    passport = require("passport")
+  mongoose = require("mongoose"),
+  passport = require("passport");
 
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
-const users = require("./routes/api/users")
-const profile = require("./routes/api/profile")
-const posts = require("./routes/api/posts")
-
-const app = express()
+const app = express();
 
 // Express middleware
-app.use(express.urlencoded({
+app.use(
+  express.urlencoded({
     extended: false
-}));
-app.use(express.json())
+  })
+);
+app.use(express.json());
 
 // DB config
-const db = require("./config/keys").mongoURI
+const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
-mongoose.connect(db, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+mongoose
+  .connect(
+    db,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 // Passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 // Passport Config
-require("./config/passport")(passport)
+require("./config/passport")(passport);
 
 // Use Routes
-app.use("/api/users", users)
-app.use("/api/profile", profile)
-app.use("/api/posts", posts)
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-
-app.listen(port, () => console.log(`Server running on port ${port}`))
+app.listen(port, () => console.log(`Server running on port ${port}`));
